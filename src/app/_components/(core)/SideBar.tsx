1e7cc2b0a30b052e6570f8616c0cd0e0c2154ea0
Moktor48 <chris.berhow@gmail.com>
2024-01-30 14:43:24 -0700

    chasing errors in TS

--- src/app/_components/(core)/SideBar.tsx
+++ src/app/_components/(core)/SideBar.tsx
@@ -1,80 +1,69 @@
 "use client"
 import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
-import { getSession } from 'next-auth/react'
-import type { Session } from 'node_modules/next-auth/core/types'
+import type { Session } from 'next-auth';
+export default function SideBar(session: { user: Session | null }, { staff }: { staff: { id: string; userId: string; admin: boolean | null; specialist: boolean | null; representative: boolean | null; highcouncil: boolean | null; guildmaster: boolean | null; } | null }) {
 
-export default function SideBar({staff}: {staff: {id: string; userId: string; admin: boolean | null; specialist: boolean | null; representative: boolean | null; highcouncil: boolean | null; guildmaster: boolean | null;} | null}) {
-  
-    const [session, setSession] = useState<Session | null>(null)
-
-//Hook attempts to pull the session
-useEffect(() => {
-    getSession().then((session) => {
-    setSession(session)
-    }) .catch((err) => {
-    console.error(err)
-    }
-)}, [])
-
-//Sidebar set-up  
-  const [sidebar, setSidebar] = useState(false) 
-  useEffect(() => {
-    const handleResize = () => {
-        if (window.innerWidth < 600 && sidebar) {
-            setSidebar(false)
+    //Sidebar set-up  
+    const [sidebar, setSidebar] = useState(false)
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth < 600 && sidebar) {
+                setSidebar(false)
+            }
         }
-    }
-    window.addEventListener('resize', handleResize)
-    return () => 
-        window.removeEventListener('resize', handleResize)
+        window.addEventListener('resize', handleResize)
+        return () =>
+            window.removeEventListener('resize', handleResize)
     }, [sidebar])
 
-// Session is not valid
-    if(!session){
-    return (
-        <div>
-            <button onClick={()=> setSidebar(!sidebar)} className="nav-button">
-                <span className="menu-button material-symbols-outlined">{sidebar? "Close":"Menu"}</span>
-            </button>
-            <nav className={`nav ${sidebar ? "nav-open":"nav-closed"}`}>
-                <ul>
-                    <li><Link className="links" href="/"><span className="material-symbols-outlined">Home</span> Home</Link></li>
-                    <li><Link className="links" href="/games"><span className="material-symbols-outlined">Casino</span> Games</Link></li>
-                    <li><Link className="links" href="/about"><span className="material-symbols-outlined">Question_Mark</span> About</Link></li>
-                    <li><Link className="links" href="/api/auth/signin"><span className="material-symbols-outlined">Login</span> Sign-in</Link></li>
-                </ul>
-            </nav>
-        </div>
-    )}
+    // Session is not valid
+    if (!session) {
+        return (
+            <div>
+                <button onClick={() => setSidebar(!sidebar)} className="nav-button">
+                    <span className="menu-button material-symbols-outlined">{sidebar ? "Close" : "Menu"}</span>
+                </button>
+                <nav className={`nav ${sidebar ? "nav-open" : "nav-closed"}`}>
+                    <ul>
+                        <li><Link className="links" href="/"><span className="material-symbols-outlined">Home</span> Home</Link></li>
+                        <li><Link className="links" href="/games"><span className="material-symbols-outlined">Casino</span> Games</Link></li>
+                        <li><Link className="links" href="/about"><span className="material-symbols-outlined">Question_Mark</span> About</Link></li>
+                        <li><Link className="links" href="/api/auth/signin"><span className="material-symbols-outlined">Login</span> Sign-in</Link></li>
+                    </ul>
+                </nav>
+            </div>
+        )
+    }
 
 
-// Session is valid, "guest" is the generic role granted to first-time users
+    // Session is valid, "guest" is the generic role granted to first-time users
     const id: string = session?.user?.id
     const role: string = session?.user?.role
     const admin = staff?.admin
 
-  
-  return (
-    <div>
-    <button onClick={()=> setSidebar(!sidebar)} className="nav-button">
-    <span className="menu-button material-symbols-outlined">{sidebar? "Close":"Menu"}</span>
-    </button>
-    <nav className={`nav ${sidebar ? "nav-open":"nav-closed"}`}>
-        <ul>
-            <li><Link className="links" href="/"><span className="material-symbols-outlined">Home</span> Home</Link></li>
-            { role != "guest" && <li><Link className="links" href={`/account/${id}`}><span className="material-symbols-outlined">Person</span> Account</Link></li>}
-            { role === "staff" && <li><Link className="links" href={`/account/${id}/staff`}><span className="material-symbols-outlined">Supervisor_Account</span> Staff</Link></li>}
-            { admin && <li><Link className="links" href={`/account/${id}/admin`}><span className="material-symbols-outlined">Admin_Panel_Settings</span> Admin</Link></li>}
-            <li><Link className="links" href="/games"><span className="material-symbols-outlined">Casino</span> Games</Link></li>            
-            <li><Link className="links" href="/about"><span className="material-symbols-outlined">Question_Mark</span> About</Link></li>
-            <li><Link className="links" href="/api/auth/signout"><span className="material-symbols-outlined">Logout</span> Sign-out</Link></li>
-        </ul>
-    </nav>
 
-    </div>
+    return (
+        <div>
+            <button onClick={() => setSidebar(!sidebar)} className="nav-button">
+                <span className="menu-button material-symbols-outlined">{sidebar ? "Close" : "Menu"}</span>
+            </button>
+            <nav className={`nav ${sidebar ? "nav-open" : "nav-closed"}`}>
+                <ul>
+                    <li><Link className="links" href="/"><span className="material-symbols-outlined">Home</span> Home</Link></li>
+                    {role != "guest" && <li><Link className="links" href={`/account/${id}`}><span className="material-symbols-outlined">Person</span> Account</Link></li>}
+                    {role === "staff" && <li><Link className="links" href={`/account/${id}/staff`}><span className="material-symbols-outlined">Supervisor_Account</span> Staff</Link></li>}
+                    {admin && <li><Link className="links" href={`/account/${id}/admin`}><span className="material-symbols-outlined">Admin_Panel_Settings</span> Admin</Link></li>}
+                    <li><Link className="links" href="/games"><span className="material-symbols-outlined">Casino</span> Games</Link></li>
+                    <li><Link className="links" href="/about"><span className="material-symbols-outlined">Question_Mark</span> About</Link></li>
+                    <li><Link className="links" href="/api/auth/signout"><span className="material-symbols-outlined">Logout</span> Sign-out</Link></li>
+                </ul>
+            </nav>
 
-)}
+        </div>
+
+    )
+}
 
 /*
 FUTURE NOTES: 
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -5,372 +5,363 @@ import {
   protectedProcedure
 } from "~/server/api/trpc";
 
-interface Permission{
-  postId: string,
-    eso: boolean,
-    ffxiv: boolean,
-    swtor: boolean,
-    general: boolean
-  
-}
-
 export const postRouter = createTRPCRouter({
   allUsers: protectedProcedure
-  .query(async () => {
-    const users = await db.user.findMany({
-    });
-    return users;
-  }),
+    .query(async () => {
+      const users = await db.user.findMany({
+      });
+      return users;
+    }),
 
- //pulls relations for a userId, can be used to determine access and profiles
+  //pulls relations for a userId, can be used to determine access and profiles
   fullProfile: protectedProcedure
-  .input(z.object({ userId: z.string() }))
-  .query(async ({ input }) => {
-    const user = await db.user.findUnique({
-      where: { id: input.userId },
-      include: {
-        eso: true,
-        swtor: true,
-        ffxiv: true,
-      }
-    });
-    return user;
-  }),
+    .input(z.object({ userId: z.string() }))
+    .query(async ({ input }) => {
+      const user = await db.user.findUnique({
+        where: { id: input.userId },
+        include: {
+          eso: true,
+          swtor: true,
+          ffxiv: true,
+        }
+      });
+      return user;
+    }),
 
-//creates a new post for approval
+  //creates a new post for approval
   post: protectedProcedure
-  .input(z.object({ createdById: z.string(), post: z.string(), title: z.string() }))
-  .mutation(async ({ input }) => {
-    const post = await db.post.create({
-      data: {
-        createdById: input.createdById,
-        post: input.post,
-        title: input.title,
-      }
-    });
-    return post;
-  }),
+    .input(z.object({ createdById: z.string(), post: z.string(), title: z.string() }))
+    .mutation(async ({ input }) => {
+      const post = await db.post.create({
+        data: {
+          createdById: input.createdById,
+          post: input.post,
+          title: input.title,
+        }
+      });
+      return post;
+    }),
 
-//This one works but makes TS mad... 
+  //This one works but makes TS mad... 
   postPermissions: protectedProcedure
-  .input(z.object({ postId: z.string(), eso: z.boolean(), ffxiv: z.boolean(), swtor: z.boolean(), general: z.boolean(), staff: z.boolean(), raid: z.boolean(), officer: z.boolean() }))
-  .mutation(async ({ input }) => {
-    const post = await db.post_permission.create({
-      data: {
-        postId: input.postId,
-        eso: input.eso,
-        ffxiv: input.ffxiv,
-        swtor: input.swtor,
-        general: input.general,
-        staff: input.staff,
-        raid: input.raid,
-        officer: input.officer,
-      }
-    });
-    return post;
-  }),
+    .input(z.object({ postId: z.string(), eso: z.boolean(), ffxiv: z.boolean(), swtor: z.boolean(), general: z.boolean(), staff: z.boolean(), raid: z.boolean(), officer: z.boolean() }))
+    .mutation(async ({ input }) => {
+      const post = await db.post_permission.create({
+        data: {
+          postId: input.postId,
+          eso: input.eso,
+          ffxiv: input.ffxiv,
+          swtor: input.swtor,
+          general: input.general,
+          staff: input.staff,
+          raid: input.raid,
+          officer: input.officer,
+        }
+      });
+      return post;
+    }),
 
-//queries all posts, other queries will include "byGuild" or "byUserId" to filter by guild or user
+  //queries all posts, other queries will include "byGuild" or "byUserId" to filter by guild or user
   getAllPosts: protectedProcedure
-  .query(async () => {
-    const posts = await db.post.findMany({
-      select: {
-        id: true,
-        title: true,
-        createdBy: {
-          select: {
-            id: true,
-            name: true,
-          }
-        },
-        permissions: {
-          select: {
-            eso: true,
-            ffxiv: true,
-            swtor: true,
-            general: true,
-          }
-        },  
-      }
-    });
+    .query(async () => {
+      const posts = await db.post.findMany({
+        select: {
+          id: true,
+          title: true,
+          createdBy: {
+            select: {
+              id: true,
+              name: true,
+            }
+          },
+          permissions: {
+            select: {
+              eso: true,
+              ffxiv: true,
+              swtor: true,
+              general: true,
+            }
+          },
+        }
+      });
       return posts;
-  }),
+    }),
 
   getPost: protectedProcedure
-  .input(z.object({ postId: z.string() }))
-  .query(async ({ input }) => {
-    const post = await db.post.findUnique({
-      where: { id: input.postId },
-      select: {
-        id: true,
-        title: true,
-        post: true,
-        createdBy: {
-          select: {
-            id: true,
-            name: true,
-          }
-        },
-        permissions: {
-          select: {
-            eso: true,
-            ffxiv: true,
-            swtor: true,
-            general: true,
-          }
-        },
-      }
-    });
-    return post;
-  }),
+    .input(z.object({ postId: z.string() }))
+    .query(async ({ input }) => {
+      const post = await db.post.findUnique({
+        where: { id: input.postId },
+        select: {
+          id: true,
+          title: true,
+          post: true,
+          createdBy: {
+            select: {
+              id: true,
+              name: true,
+            }
+          },
+          permissions: {
+            select: {
+              eso: true,
+              ffxiv: true,
+              swtor: true,
+              general: true,
+            }
+          },
+        }
+      });
+      return post;
+    }),
 
-//Create
+  //Create
   createUser: protectedProcedure
-  .input(z.object({ name: z.string(), email: z.string(), role: z.string(), image: z.string() }))
-  .mutation(async ({ input }) => {
-    const user = await db.user.create({
-      data: {
-        name: input.name,
-        email: input.email,
-        role: input.role,
-        image: input.image,
-      }
-    });
-    return user;
-  }),
+    .input(z.object({ name: z.string(), email: z.string(), role: z.string(), image: z.string() }))
+    .mutation(async ({ input }) => {
+      const user = await db.user.create({
+        data: {
+          name: input.name,
+          email: input.email,
+          role: input.role,
+          image: input.image,
+        }
+      });
+      return user;
+    }),
 
   createStaffPermission: protectedProcedure
-  .input(z.object({ userId: z.string(), admin: z.boolean(), specialist: z.boolean(), representative: z.boolean(), highcouncil: z.boolean(), guildmaster: z.boolean() }))
-  .mutation(async ({ input }) => {
-    const staff = await db.staff.create({
-      data: {
-        userId: input.userId,
-        admin: input.admin,
-        specialist: input.specialist,
-        representative: input.representative,
-        highcouncil: input.highcouncil,
-        guildmaster: input.guildmaster,
-      }
-    });
-    return staff;
-  }),
+    .input(z.object({ userId: z.string(), admin: z.boolean(), specialist: z.boolean(), representative: z.boolean(), highcouncil: z.boolean(), guildmaster: z.boolean() }))
+    .mutation(async ({ input }) => {
+      const staff = await db.staff.create({
+        data: {
+          userId: input.userId,
+          admin: input.admin,
+          specialist: input.specialist,
+          representative: input.representative,
+          highcouncil: input.highcouncil,
+          guildmaster: input.guildmaster,
+        }
+      });
+      return staff;
+    }),
 
   createEsoPermission: protectedProcedure
-  .input(z.object({ userId: z.string(), rank: z.string(), raid: z.boolean(), raidlead: z.boolean(), mentor: z.boolean()}))
-  .mutation(async ({ input }) => {
-    const eso = await db.eso.create({
-      data: {
-        userId: input.userId,
-        rank: input.rank,
-        raid: input.raid,
-        raidlead: input.raidlead,
-        mentor: input.mentor,
-      }
-    });
-    return eso;
-  }),
+    .input(z.object({ userId: z.string(), rank: z.string(), raid: z.boolean(), raidlead: z.boolean(), mentor: z.boolean() }))
+    .mutation(async ({ input }) => {
+      const eso = await db.eso.create({
+        data: {
+          userId: input.userId,
+          rank: input.rank,
+          raid: input.raid,
+          raidlead: input.raidlead,
+          mentor: input.mentor,
+        }
+      });
+      return eso;
+    }),
 
   createSwtorPermission: protectedProcedure
-  .input(z.object({ userId: z.string(), rank: z.string(), raid: z.boolean(), raidlead: z.boolean(), mentor: z.boolean()}))
-  .mutation(async ({ input }) => {
-    const swtor = await db.swtor.create({
-      data: {
-        userId: input.userId,
-        rank: input.rank,
-        raid: input.raid,
-        raidlead: input.raidlead,
-        mentor: input.mentor,
-      }
-    });
-    return swtor;
-  }),
+    .input(z.object({ userId: z.string(), rank: z.string(), raid: z.boolean(), raidlead: z.boolean(), mentor: z.boolean() }))
+    .mutation(async ({ input }) => {
+      const swtor = await db.swtor.create({
+        data: {
+          userId: input.userId,
+          rank: input.rank,
+          raid: input.raid,
+          raidlead: input.raidlead,
+          mentor: input.mentor,
+        }
+      });
+      return swtor;
+    }),
 
   createFfxivPermission: protectedProcedure
-  .input(z.object({ userId: z.string(), rank: z.string(), raid: z.boolean(), raidlead: z.boolean(), mentor: z.boolean()}))
-  .mutation(async ({ input }) => {
-    const ffxiv = await db.ffxiv.create({
-      data: {
-        userId: input.userId,
-        rank: input.rank,
-        raid: input.raid,
-        raidlead: input.raidlead,
-        mentor: input.mentor,
-      }
-    });
-    return ffxiv;
-  }),
+    .input(z.object({ userId: z.string(), rank: z.string(), raid: z.boolean(), raidlead: z.boolean(), mentor: z.boolean() }))
+    .mutation(async ({ input }) => {
+      const ffxiv = await db.ffxiv.create({
+        data: {
+          userId: input.userId,
+          rank: input.rank,
+          raid: input.raid,
+          raidlead: input.raidlead,
+          mentor: input.mentor,
+        }
+      });
+      return ffxiv;
+    }),
 
-//Query Permissions
+  //Query Permissions
   staffPermission: protectedProcedure
-  .input(z.object({ userId: z.string() }))
-  .query(async ({ input }) => {
-    const user = await db.staff.findUnique({
-      where: { userId: input.userId },
-    });
-    return user;
-  }),
+    .input(z.object({ userId: z.string() }))
+    .query(async ({ input }) => {
+      const user = await db.staff.findUnique({
+        where: { userId: input.userId },
+      });
+      return user;
+    }),
 
   esoPermission: protectedProcedure
-  .input(z.object({ userId: z.string() }))
-  .query(async ({ input }) => {
-    const user = await db.eso.findUnique({
-      where: { userId: input.userId },
-    });
-    return user;
-  }),
+    .input(z.object({ userId: z.string() }))
+    .query(async ({ input }) => {
+      const user = await db.eso.findUnique({
+        where: { userId: input.userId },
+      });
+      return user;
+    }),
 
   swtorPermission: protectedProcedure
-  .input(z.object({ userId: z.string() }))
-  .query(async ({ input }) => {
-    const user = await db.swtor.findUnique({
-      where: { userId: input.userId },
-    });
-    return user;
-  }),
+    .input(z.object({ userId: z.string() }))
+    .query(async ({ input }) => {
+      const user = await db.swtor.findUnique({
+        where: { userId: input.userId },
+      });
+      return user;
+    }),
 
   ffxivPermission: protectedProcedure
-  .input(z.object({ userId: z.string() }))
-  .query(async ({ input }) => {
-    const user = await db.ffxiv.findUnique({
-      where: { userId: input.userId },
-    });
-    return user;
-  }),
+    .input(z.object({ userId: z.string() }))
+    .query(async ({ input }) => {
+      const user = await db.ffxiv.findUnique({
+        where: { userId: input.userId },
+      });
+      return user;
+    }),
 
   //Post queries
   unpublishedPosts: protectedProcedure
-  .query(async () => {
-    const posts = await db.post.findMany({
-      where: { published: false },
-      select: {
-        id: true,
-        title: true,
-        createdBy: {
-          select: {
-            id: true,
-            name: true,
-          }
-        },
-        permissions: {
-          select: {
-            eso: true,
-            ffxiv: true,
-            swtor: true,
-            general: true,
-          }
-        },
-      }
-    });
-    return posts;
-  }),
+    .query(async () => {
+      const posts = await db.post.findMany({
+        where: { published: false },
+        select: {
+          id: true,
+          title: true,
+          createdBy: {
+            select: {
+              id: true,
+              name: true,
+            }
+          },
+          permissions: {
+            select: {
+              eso: true,
+              ffxiv: true,
+              swtor: true,
+              general: true,
+            }
+          },
+        }
+      });
+      return posts;
+    }),
 
   unpublishedPostsEso: protectedProcedure
-  .query(async () => {
-    const posts = await db.post.findMany({
-      where: { published: false },
-      select: {
-        id: true,
-        title: true,
-        createdBy: {
-          select: {
-            id: true,
-            name: true,
-          }
-        },
-        permissions: {
-          select: {
-            eso: true,
-            ffxiv: false,
-            swtor: false,
-            general: false,
-          }
-        },
-      }
-    });
-    return posts;
-  }),
+    .query(async () => {
+      const posts = await db.post.findMany({
+        where: { published: false },
+        select: {
+          id: true,
+          title: true,
+          createdBy: {
+            select: {
+              id: true,
+              name: true,
+            }
+          },
+          permissions: {
+            select: {
+              eso: true,
+              ffxiv: false,
+              swtor: false,
+              general: false,
+            }
+          },
+        }
+      });
+      return posts;
+    }),
 
   unpublishedPostsFfxiv: protectedProcedure
-  .query(async () => {
-    const posts = await db.post.findMany({
-      where: { published: false },
-      select: {
-        id: true,
-        title: true,
-        createdBy: {
-          select: {
-            id: true,
-            name: true,
-          }
-        },
-        permissions: {
-          select: {
-            eso: false,
-            ffxiv: true,
-            swtor: false,
-            general: false,
-          }
-        },
-      }
-    });
-    return posts;
-  }),
+    .query(async () => {
+      const posts = await db.post.findMany({
+        where: { published: false },
+        select: {
+          id: true,
+          title: true,
+          createdBy: {
+            select: {
+              id: true,
+              name: true,
+            }
+          },
+          permissions: {
+            select: {
+              eso: false,
+              ffxiv: true,
+              swtor: false,
+              general: false,
+            }
+          },
+        }
+      });
+      return posts;
+    }),
 
-    unpublishedPostsSwtor: protectedProcedure
-  .query(async () => {
-    const posts = await db.post.findMany({
-      where: { published: false },
-      select: {
-        id: true,
-        title: true,
-        createdBy: {
-          select: {
-            id: true,
-            name: true,
-          }
-        },
-        permissions: {
-          select: {
-            eso: false,
-            ffxiv: false,
-            swtor: true,
-            general: false,
-          }
-        },
-      }
-    });
-    return posts;
-  }),
+  unpublishedPostsSwtor: protectedProcedure
+    .query(async () => {
+      const posts = await db.post.findMany({
+        where: { published: false },
+        select: {
+          id: true,
+          title: true,
+          createdBy: {
+            select: {
+              id: true,
+              name: true,
+            }
+          },
+          permissions: {
+            select: {
+              eso: false,
+              ffxiv: false,
+              swtor: true,
+              general: false,
+            }
+          },
+        }
+      });
+      return posts;
+    }),
 
   displayPost: protectedProcedure
-  .input(z.object({ postId: z.string() }))
-  .query(async ({ input }) => {
-    const post = await db.post.findUnique({
-      where: { id: input.postId },
-      select: {
-        id: true,
-        title: true,
-        post: true,
-        createdBy: {
-          select: {
-            id: true,
-            name: true,
-            image: true,
-          }
-        },
-        published: true,
-        permissions: {
-          select: {
-            eso: true,
-            ffxiv: true,
-            swtor: true,
-            general: true,
-          }
-        },
-      }
-    });
-    return post;
-  })
+    .input(z.object({ postId: z.string() }))
+    .query(async ({ input }) => {
+      const post = await db.post.findUnique({
+        where: { id: input.postId },
+        select: {
+          id: true,
+          title: true,
+          post: true,
+          createdBy: {
+            select: {
+              id: true,
+              name: true,
+              image: true,
+            }
+          },
+          published: true,
+          permissions: {
+            select: {
+              eso: true,
+              ffxiv: true,
+              swtor: true,
+              general: true,
+            }
+          },
+        }
+      });
+      return post;
+    })
 
 }) // This is the end, lawlz. 
 
