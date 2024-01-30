cfcb421a3cdce90337d3b880b94e9cb21a02ece2
Moktor48 <chris.berhow@gmail.com>
2024-01-30 10:34:29 -0700

    clean up more types

--- src/app/_components/(postApproval)/EsoPosts.tsx
+++ src/app/_components/(postApproval)/EsoPosts.tsx
@@ -5,24 +5,24 @@ import React from 'react'
 import Link from "next/link";
 
 export default async function EsoPosts() {
-    const session = await getServerAuthSession()
-    if (!session) return <div>You must be logged in to view this page.</div>
-    const id = session.user.id
-    const esoPerms = await api.post.esoPermission.query({userId: id})
-    if (!esoPerms) return <div>You must be a member of the ESO Discord to view this page.</div>
-    if (!esoPerms.raidlead && !esoPerms.mentor && esoPerms.rank != "officer") return null
-    const unpubPostEso = await api.post.unpublishedPostsEso.query()
-    console.log(unpubPostEso)
+  const session = await getServerAuthSession()
+  if (!session) return <div>You must be logged in to view this page.</div>
+  const id = session.user.id
+  const esoPerms = await api.post.esoPermission.query({ userId: id })
+  if (!esoPerms) return <div>You must be a member of the ESO Discord to view this page.</div>
+  if (!esoPerms.raidlead && !esoPerms.mentor && esoPerms.rank != "officer") return null
+  const unpubPostEso = await api.post.unpublishedPostsEso.query()
+  console.log(unpubPostEso)
   return (
     <div>
-        <h1 className="text-white">UnPublished Posts for ESO</h1>
-        {unpubPostEso.map((post) => {
+      <h1 className="text-white">UnPublished Posts for ESO</h1>
+      {unpubPostEso.map((post: { id: React.Key | null | undefined; title: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; createdBy: { name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; }; }) => {
         return (
-                <div>
-                    <Link href={`./approve/${post.id}`}><p key={post.id}>Title: {post.title} Created by: {post.createdBy.name}</p></Link>
-                </div>
-            )
-        })}
+          <div>
+            <Link href={`./approve/${post.id}`}><p key={post.id}>Title: {post.title} Created by: {post.createdBy.name}</p></Link>
+          </div>
+        )
+      })}
     </div>
   )
 }
--- src/app/_components/(postApproval)/SwtorPosts.tsx
+++ src/app/_components/(postApproval)/SwtorPosts.tsx
@@ -7,24 +7,24 @@ export default async function SwtorPosts() {
     const session = await getServerAuthSession()
     if (!session) return <div>You must be logged in to view this page.</div>
     const id = session.user.id
-    const swtorPerms = await api.post.swtorPermission.query({userId: id})
+    const swtorPerms = await api.post.swtorPermission.query({ userId: id })
     if (!swtorPerms) return <div>You must be a member of the Swtor Discord to view this page.</div>
     if (!swtorPerms.raidlead && !swtorPerms.mentor && swtorPerms.rank != "officer") return null
     const unpubPostSwtor = await api.post.unpublishedPostsSwtor.query()
-  return (
-    <div>
-        {unpubPostSwtor.map((post) => {
-        return (
-                <div>
-                    <p>
-                        Title: {post.title}, 
-                    </p>
-                    <p>
-                        Created by: {post.createdBy.name}
-                    </p>
-                </div>
-            )
-        })}
-    </div>
-  )
+    return (
+        <div>
+            {unpubPostSwtor.map((post: { title: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; createdBy: { name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; }; }) => {
+                return (
+                    <div>
+                        <p>
+                            Title: {post.title},
+                        </p>
+                        <p>
+                            Created by: {post.createdBy.name}
+                        </p>
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
