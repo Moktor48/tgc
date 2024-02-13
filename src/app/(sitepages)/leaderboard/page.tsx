39e78b0798cbf2b7ddefd87f3d609e7c06a44597
Masterevan27 <masterevan27@gmail.com>
2024-02-13 18:06:20 -0500

    began work on the leaderboard page

---
+++ src/app/(sitepages)/leaderboard/page.tsx
@@ -0,0 +1,68 @@
+// doesnt seem to be used:
+// import Link from "next/link";
+import React from "react";
+import NavBar from "~/app/_components/(core)/NavBar";
+import "~/styles/leaderboard.css";
+
+export default function LeaderboardPage() {
+  return (
+    <main>
+    <NavBar />
+
+    <form method="post" action="" className="leaderboard-form">
+      <label for="start_date" className="font-bold">Start Date:</label>
+      <input type="date" id="start_date" name="start_date" value="2024-01-30" className="leaderboard-input-date"/>
+
+      <label for="end_date" className="font-bold"> End Date:</label>
+      <input type="date" id="end_date" name="end_date" value="2024-02-01" className="leaderboard-input-date"/>
+
+      <input type="submit" value="Submit" className="btn btn-success leaderboard-btn"/>
+    </form>
+
+    <div className="leaderboard font-bold text-xl">
+      <h2 className="leaderboard-title">Leaderboard</h2>
+      <div className="leaderboard-table overflow-x-auto">
+        <div className="white-line-bar"></div>
+        <table className="table leaderboard-content text-base">
+            <thead className="leaderboard-headers font-bold text-lg">
+                <tr>
+                  <th>User</th>
+                  <th>Rank</th>
+                  <th>Points</th>
+                  <th>Voice Duration</th>
+                  <th>Messages</th>
+                  <th>Passed</th>
+                </tr>
+            </thead>
+            <tbody>
+              <tr className="hover">
+                <td>User1</td>
+                <td>Respected</td>
+                <td>497</td>
+                <td>36.19</td>
+                <td>0</td>
+                <td>Yes</td>
+              </tr>
+              <tr className="hover">
+                <td>User2</td>
+                <td>High Council	</td>
+                <td>3717</td>
+                <td>47.19</td>
+                <td>0</td>
+                <td>Yes</td>
+              </tr>
+              <tr className="hover">
+                <td>User3</td>
+                <td>Respected</td>
+                <td>359</td>
+                <td>0.00</td>
+                <td>0</td>
+                <td>Yes</td>
+              </tr>
+            </tbody>
+        </table>
+      </div>
+    </div>
+    </main>
+  );
+}
