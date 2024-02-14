fb93c7c576f3d8b0caf21a4e5f40bf01e4a91680
moktor48 <chris.berhow@gmail.com>
2024-02-13 23:34:46 -0700

    NavBar cleanup, same throughout the site, staff permissions appear when relevant

--- src/app/(sitepages)/games/[game]/[id]/page.tsx
+++ src/app/(sitepages)/games/[game]/[id]/page.tsx
@@ -1,10 +1,6 @@
 import Link from "next/link";
 import React from "react";
-import NavBarEso from "~/app/_components/(gameComponents)/(eso)/NavBarEso";
-import NavBarFfxiv from "~/app/_components/(gameComponents)/(ffxiv)/NavBarFfxiv";
-import NavBarSwtor from "~/app/_components/(gameComponents)/(swtor)/NavBarSwtor";
 import { getServerAuthSession } from "~/server/auth";
-import { api } from "~/trpc/server";
 
 export default async function page({
   params,
@@ -12,20 +8,11 @@ export default async function page({
   params: { game: string; id: string };
 }) {
   const session = await getServerAuthSession();
-  if (!session) return null;
+  if (!session) return <div>You must be logged in to view this page.</div>;
   const id = params.id;
-  const game = params.game;
-  const perm = await api.get.staffPermission.query({ userId: id });
 
   return (
     <>
-      {game === "eso" && <NavBarEso session={session} id={id} perm={perm} />}
-      {game === "ffxiv" && (
-        <NavBarFfxiv session={session} id={id} perm={perm} />
-      )}
-      {game === "swtor" && (
-        <NavBarSwtor session={session} id={id} perm={perm} />
-      )}
       <div>
         <p className="text-white">Hello {session.user.name}!</p>
         <p className="text-3xl text-white">Current published posts</p>
--- src/app/_components/(core)/NavBar.tsx
+++ src/app/_components/(core)/NavBar.tsx
@@ -1,7 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
 
-export default async function NavBar() {
+export default async function NavBar({
+  role,
+  id,
+}: {
+  role: string;
+  id: string;
+}) {
   return (
     <div className="navbar" id="navbar">
       <div className="dropdown">
@@ -52,6 +58,21 @@ export default async function NavBar() {
           <Link href="/article/ffxiv">FFXIV</Link>
         </div>
       </div>
+
+      {role === "staff" && (
+        <div className="dropdown">
+          <button className="dropbtn">
+            <h2>
+              Staff<span className="triangle-down"></span>
+            </h2>
+          </button>
+          <div className="nav-dropdown-content">
+            <Link href={`/dashboard/${id}/staff`}>Staff</Link>
+            <Link href={`/dashboard/${id}/admin`}>Admin</Link>
+            <Link href={`/editor/${id}/approve`}>Post Approvals</Link>
+          </div>
+        </div>
+      )}
 
       <a href="https://discord.gg/TGC" className="dropbtn" target="_blank">
         <Image
