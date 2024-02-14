6021a04ce005b145d12b0b50a8a72c4096c3eb91
moktor48 <chris.berhow@gmail.com>
2024-02-14 06:54:17 -0700

    Coffee kicking in, NavBars should be clean now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 //This is the home page, think of this as the index.html/php/etc
 import FrontContent from "./_components/(core)/FrontContent";
-import NavBar from "./_components/(core)/NavBar";
 import WarningBanner from "./_components/(core)/WarningBanner";
 
 export default async function Home() {
   return (
     <main>
-      <NavBar />
       <WarningBanner />
       <div className="header-container">
         <div className="header">
@@ -20,10 +18,3 @@ export default async function Home() {
     </main>
   );
 }
-
-/*
-FUTURE NOTES: 
-        <Layout navbarType={0}> // will display navbar 1
-          <p>Im page 1</p>
-        </Layout>
-*/
