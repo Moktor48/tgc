30e3ef78fca778131c2c96e0ac13d70d94050504
moktor <chris.berhow@gmail.com>
2024-04-12 15:20:11 -0700

    Warning banner

--- src/app/_components/(core)/WarningBanner.tsx
+++ src/app/_components/(core)/WarningBanner.tsx
@@ -4,95 +4,18 @@ export default function WarningBanner() {
   return (
     <div className="bg-black text-center text-3xl text-red-500">
       <p className="text-yellow-500">
-        Latest update: <span className="test-green-500">2/15/24@2300MST</span>
+        Latest update: <span className="test-green-500">4/12/24@1500MST</span>
       </p>
 
       <h1>Latest updates</h1>
-      <ul>
-        <div className="text-green-500">
-          <p>
-            {" "}
-            Images added, can be added to, saved with, and retrieved. There are
-            some routing bugs, especially with "general" posts. Stick with
-            ESO/SWTOR/FFXIV for best results.{" "}
-          </p>
-          <p>
-            I added a bunch of code to make the NavBar public/private, and added
-            in public-facing posts. These are posts that can be viewed by people
-            OUTSIDE the guild, or rather, not logged in. Feel free to pressure
-            test these things.
-          </p>
-          <p>
-            Articles/builds/guides are now routing properly to be read. Test
-            things out, but the EXPECTED outcome is:
-          </p>
-          <p>1. Staff - Create Post </p>
-          <p>2.Post Setup, choose the parameters </p>
-          <p>3. Create your post, and submit </p>
-          <p>
-            4. Staff member reviews (you should be able to review your own at
-            the moment){" "}
-          </p>
-          <p>5. Post should be published in the proper location.</p>
-          <p className="text-yellow-500">
-            Styling is crap at the moment, you were warned! I wanted to get the
-            functionality up, make sure the logic is working. Then I will make
-            it so your eyes don't bleed anymore!
-          </p>
-          <p className="text-red-500">WATCH for bugs, report them!</p>
-        </div>
-
-        <li className="text-green-500">
-          Log-in automatically checks Discord channel to verify membership, then
-          updates the user record. Styling is not final, just function.
-        </li>
-
-        <li className="text-green-500">NavBars homogenized</li>
-
-        <li className="text-5xl text-yellow-500">
-          REMOVED YELLOW FOR CHANCE!!!!
-        </li>
-
-        <li className="text-5xl text-yellow-500">
-          Rolled back a bit to fix the build
-        </li>
-      </ul>
-      <h1>Dashboard/Editor Directions</h1>
-      <p className="text-red-500">
-        Dashboard allows for staff to make certain changes for users, so guild
-        officers can promote their own members, etc. Much of this functionality
-        will fall on the Discord bot eventually, but this gives us a tool now
-        for changes.
-      </p>
-
-      <p className="text-red-500">
-        Refer to the text editor and test it out, many features can be added to
-        it, let me know the priorities. It can be found in the side bar (upper
-        right menu), dashboard, admin and staff sections. Templates should be
-        made for the editor, for builds, articles, reports, whatever. These can
-        be added in and I can add features to the editor as needed Permissions
-        need to be done by hand at the moment, if this is your first time
-        signing in, message moktor in Discord.
-      </p>
-
-      <h1>Current issues</h1>
-
-      <p className="text-red-500">
-        I am currently working on the means of in-app player permission mods. I
-        am currently controlling authorization for: guest/member/staff
-        [eso/ffxiv/swtor]: none/member/officer (tracked for each game) Staff
-        positions: admin/specialist/mentor/representative/HC/GM
-      </p>
-
-      <h1>More Issues?</h1>
-
-      <p className="text-red-500">
-        {" "}
-        These are just on my plate right now, if there are other priorities I'll
-        adjust fire. Suggestions can be made in the text editor, or by
-        contacting me in Discord. If you see a security flaw, let us know
-        immediately!
-      </p>
+      <p> Near total rebuild, back to TinyMCE</p>
+      <p> What WORKS?</p>
+      <p>Adding staff tracker files will parse and go to database</p>
+      <p>Leaderboard is tracking</p>
+      <p>Modding member permissions are back in</p>
+      <p>Editor ahould function, to write and save posts to database</p>
+      <p>What DOESN'T work?</p>
+      <p>Editor approvals and display are coming son</p>
     </div>
   );
 }
