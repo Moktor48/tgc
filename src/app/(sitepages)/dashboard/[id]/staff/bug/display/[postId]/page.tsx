1a64632486958c03f2b2ea89d0d9ed7fb8a2769a
moktor <chris.berhow@gmail.com>
2024-07-07 17:38:06 -0700

    Oops

--- src/app/(sitepages)/dashboard/[id]/staff/bug/display/[postId]/page.tsx
+++ src/app/(sitepages)/dashboard/[id]/staff/bug/display/[postId]/page.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { api } from "~/trpc/server";
 
-import TinyMCEDisplayBug from "~/app/_components/(tinymce)/TinyMCEDisplay";
-
+import TinyMCEDisplayBug from "~/app/_components/(tinymce)/TinyMCEDisplayBug";
+type BugType = {
+  id: string;
+  title: string;
+  content: string;
+  createdBy: {
+    name: string;
+    id: string;
+  };
+} | null;
 export default async function page({
   params,
 }: {
   params: { id: string; postId: string };
 }) {
   const postId = params.postId;
-  const display = await api.get.getBug.query({ postId: postId });
+  const display = (await api.get.getBug.query({ postId: postId })) as BugType;
   if (!display) return <p>No Data!</p>;
   return (
     <div>
